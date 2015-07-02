2cb5d41ee7f1b597e5b524249fff96eec21432e9
mastermalone <michael.malone@aofl-00015.aofl.local>
2015-07-02 16:48:49 -0700

    Adding new loop for jigsaw cutout creation.

--- scripts/modules/Jigsaw.js
+++ scripts/modules/Jigsaw.js
@@ -22,13 +22,15 @@
                 var numPieces = this.data.puzzle['level'+this.level].pieces,
                     frag = document.createDocumentFragment(), 
                     width,
-                    height, 
+                    height,
+                    imgXValue = 0,
+                    imgYValue = 0, 
                     canvas, 
                     ctx, 
                     img, 
                     evt;
                 
-                img = document.createElement('img');
+                img = new Image();
                 img.src = this.data.image;
                 console.log('Number of pieces', this.data.image, this.data);
                 console.log('Number of ', numPieces);
@@ -36,23 +38,53 @@
                 img.onload = function () {
                     for (var i = 0; i < numPieces; i++) {
                         canvas = document.createElement('canvas');
+                        canvas.className = 'jigsaw-piece';
+                        canvas.width = (e.data.width/numPieces)*2;
+                        canvas.height = (e.data.height/numPieces)*2;
+                        canvas.style.border = 'solid 1px #ff0000';
                         ctx = canvas.getContext('2d');
                         
                         ctx.save();
-                        //ctx.bezierCurveTo(20,100,200,100,200,20); Create besier curve based on random and only from the right until the last image from the ight is made
-                        ctx.drawImage(img, 0, 0);
+                        ctx.bezierCurveTo(20,100,200,100,200,20);// Create besier curve based on random and only from the right until the last image from the ight is made
+                        //ctx.drawImage(img, , (e.data.height) * i, e.data.width, e.data.height);
+                        imgXValue = (canvas.width) * i;
+                        
+                        if (((canvas.width *i) - (canvas.width)) >= e.data.width) {
+                            imgYValue += canvas.height;
+                            console.log('Greater than or equal to width:', ((canvas.width *i) - (canvas.width)), 'Total Width:',  e.data.width, imgYValue);
+                            //imgXValue = 0;
+                        }
+                        
+                        //Create an object {} that stores the values of the image background position for each puzzle piece.  In this loop, I can reference that object and set each piece there.
+                        if (((canvas.height *i) - (canvas.height)) >= e.data.height) {
+                            //imgXValue += canvas.width;
+                            console.log('Greater than or equal to height:', ((canvas.height *i) - (canvas.height)), 'Total height:',  e.data.height, imgXValue);
+                            //imgXValue = 0;
+                        }
+                        
+                        
+                        ctx.drawImage(img, (-imgXValue), (-imgYValue), e.data.width, e.data.height);
+                        
+                        
+                        /*for (var j = i; i < imgXValue; j++) {
+                            //ctx.drawImage(img, (-imgXValue), (-imgYValue), e.data.width, e.data.height);
+                        }*/
                         //DO stuff
-                        console.log('Value of i:', i);
+                        console.log('Value of width:', canvas.width, 'Height:', canvas.height);
+                        frag.appendChild(canvas);
                     }
+                    this.appendTo('main', frag);
                    
-                };
+                }.bind(this);
             },
+            
             getLevel: function (e) {
                 this.level = e.target.id.substring(10, parseInt(e.target.id.length));
                 console.log("GETTING THE LEVEL TARGET NUMBER:", this.level);
             },
-            appendTo: function (el) {
+            appendTo: function (el, child) {
                 var parent = typeof el === 'string' ? document.getElementById(el) : el;
+                parent.appendChild(child);
             }
         };
         
